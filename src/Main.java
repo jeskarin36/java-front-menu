1629b8aa8747a8c645888c1a602b7fbc173b4bbe
jeskarin36 <94501234+jeskarin36@users.noreply.github.com>
2023-02-02 10:59:13 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+import dspswing.Home;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author morochas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Home h=new Home();
+         h.setVisible(true);
+         h.setSize(1000,600);
+         h.setLocationRelativeTo(null);
+    }
+    
+}
